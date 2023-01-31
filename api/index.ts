e27593c8241944c4cb2b59635ff54168ced7faee
Haroen <hello@haroen.me>
2023-01-31 23:38:03 +0100

    actually implement

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,21 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import { hammerheadDashboardLogin, hammerheadSync } from '../lib/hammerhead';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { username, password } = req.body;
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const { username, password } = JSON.parse(req.body);
+
+  console.log(req.body, username, password)
 
   if (!username || !password) {
     return res.status(400).json({ message: 'Missing username or password' });
   }
 
-  return res.json({ message: 'Hello World' });
+  const auth = await hammerheadDashboardLogin(username, password);
+  const sync = await hammerheadSync(auth);
+
+  return res.json(sync);
 }
